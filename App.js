8fd5dc96d9715b50e273802fc638711ecb511740
Filip Vitas <filip.vitas@sbgenomics.com>
2018-01-05 00:08:29 +0100

    make car turning

--- App.js
+++ App.js
@@ -1,52 +1,96 @@
+import React, { Component } from 'react'
+import { Platform, Dimensions, StyleSheet, View, TouchableWithoutFeedback} from 'react-native'
+import Svg, { Ellipse, Circle, Rect, G, Line, Text } from 'react-native-svg'
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-})
+const window = Dimensions.get('window');
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    )
-  }
+
+    constructor() {
+        super()
+
+        this.state = {
+            x: window.width / 8,
+            y: window.height / 2,
+            deltaX: 0,
+            deltaY: -1,
+            turnAngle: -Math.PI / 2,
+            rotation: 0
+        }
+
+        this.tick = this.tick.bind(this)
+        this.onTouchStart = this.onTouchStart.bind(this)
+        this.onTouchEnd = this.onTouchEnd.bind(this)
+    }
+
+    componentDidMount() {
+        requestAnimationFrame(this.tick)
+    }
+
+    componentWillUnmount() {
+        cancelAnimationFrame(this.tick)
+    }
+
+    tick() {
+        let { x, y, deltaX, deltaY, turnAngle, rotation } = this.state
+        let acceleration = 3.5
+
+        if (this.touched) {
+            this.angle = Math.min(this.angle + 0.01,  2)
+            rotation = (rotation + this.angle) % 360
+
+            turnAngle = (turnAngle + this.angle * Math.PI / 180) % 360
+
+            deltaX = Math.cos(turnAngle)
+            deltaY = 0.8 * Math.sin(turnAngle)
+
+            // to make it drift more, i must include inertia and more rotation in equation
+            // more complex then this
+            // rotation += 1
+
+        } else {
+            this.angle = 1.5
+        }
+
+        this.setState(prevState => ({
+            x: Number((x + acceleration * deltaX).toFixed(2)),
+            y: Number((y + acceleration * deltaY).toFixed(2)),
+            deltaX,
+            deltaY,
+            turnAngle,
+            rotation
+        }))
+
+        requestAnimationFrame(this.tick)
+    }
+
+    onTouchStart() {
+        this.touched = true
+    }
+
+    onTouchEnd() {
+        this.touched = false
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TouchableWithoutFeedback onPressIn={this.onTouchStart} onPressOut={this.onTouchEnd}>
+                    <Svg width='300' height='500'>
+                        <Rect x='50' y='50' width='200' height='400' rx='100' ry='100' stroke='#9FA8DA' strokeWidth='90' fill='none'/>
+                        <Rect x={this.state.x} y={this.state.y} width='18' height='30' fill='black' rx='5' ry='5' origin={`${this.state.x},${this.state.y}`} rotation={this.state.rotation}/>
+                    </Svg>
+                </TouchableWithoutFeedback>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#eee'
+    }
 })
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ReactNativeTurnRight
-React Native Turn Right Game
+
+
+### Generate and sign android apk
+
+- add local.properties file in /android folder
+    ```
+     sdk.dir = /Users/nameHere/Library/Android/sdk
+    ```
+    
+- add keystore.properties file in /android folder
+   ```
+   storeFile=release-key.keystore
+   keyAlias=release-key-alias
+   storePassword=***
+   keyPassword=***
+   ```
+
+- generate .keystore file and place it under /android/app folder
+    ```
+    sudo keytool -genkey -v -keystore release-key.keystore -alias release-key-alias -keyalg RSA -keysize 2048 -validity 10000
+    ```
+    
+- run command 
+    ```
+    cd android && ./gradlew assembleRelease
+    ```
